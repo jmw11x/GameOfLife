57328afaa6f264bd45036a62fefb36d96e606b1e
Priscilla Martinez <60373527+priscillaam@users.noreply.github.com>
2021-04-17 11:01:32 -0400

    Count Neighbors
    - counts neighbors of all cells alive when hit play

--- script.js
+++ script.js
@@ -8,15 +8,17 @@ function genGrid(){
     let row = document.createElement("tr");
     grid.appendChild(row);
     for(var i = 0; i<rows; i++){
-
+ 
         let row = grid.insertRow();
-
+ 
         
         
         for(var j = 0; j < cols; j++){
             let cell = document.createElement("td");   
-            cell.setAttribute('id', i + ","+j);  
-
+            cell.setAttribute('id', i+","+ j);
+            
+            cell.append(i+","+j);
+            
             cell.onclick = e =>{
                 //console.log(e.target.id);
                 
@@ -28,23 +30,24 @@ function genGrid(){
                     document.getElementById(e.target.id).style.backgroundColor = "white";
                 }
                 //console.log(this.id);
+                
             }
             row.appendChild(cell);
      
-
+ 
             // console.log(cell.className); 
         } 
         grid.appendChild(row);
    
     }
 }
-
+ 
  
 function play(){
         var block= document.getElementById("block");
         var blinker= document.getElementById("blinker");
         var beacon = document.getElementById("beacon");
-
+ 
         if(block.checked){
             map["24,24"] = 1;
             document.getElementById("24,24").style.backgroundColor = "blue";
@@ -54,7 +57,7 @@ function play(){
             document.getElementById("25,24").style.backgroundColor = "blue";
             map["25,25"] = 1;
             document.getElementById("25,25").style.backgroundColor = "blue";
-
+ 
             //these parts of the other patterns should be turned off on play
             map["26,26"] = 0;
             document.getElementById("26,26").style.backgroundColor = "white";
@@ -64,19 +67,19 @@ function play(){
             document.getElementById("27,26").style.backgroundColor = "white";
             map["27,27"] = 0;
             document.getElementById("27,27").style.backgroundColor = "white";
-
+ 
             map["26,25"] = 0;
             document.getElementById("26,25").style.backgroundColor = "white";
         }else if(blinker.checked){
             map["24,25"] = 1;
             document.getElementById("24,25").style.backgroundColor = "blue";
             map["25,25"] = 1;
-
+ 
             document.getElementById("25,25").style.backgroundColor = "blue";
             map["26,25"] = 1;
-
+ 
             document.getElementById("26,25").style.backgroundColor = "blue";
-
+ 
             //these parts of the other patterns should be turned off on play
             map["26,26"] = 0;
             document.getElementById("26,26").style.backgroundColor = "white";
@@ -86,78 +89,156 @@ function play(){
             document.getElementById("27,26").style.backgroundColor = "white";
             map["27,27"] = 0;
             document.getElementById("27,27").style.backgroundColor = "white";
-
+ 
             map["25,24"] = 0;
             document.getElementById("25,24").style.backgroundColor = "white";
             map["24,24"] = 0;
             document.getElementById("24,24").style.backgroundColor = "white";
         }else if(beacon.checked){
             map["24,24"] = 1;
-
+ 
             document.getElementById("24,24").style.backgroundColor = "blue";
             map["24,25"] = 1;
-
+ 
             document.getElementById("24,25").style.backgroundColor = "blue";
             map["25,24"] = 1;
-
+ 
             document.getElementById("25,24").style.backgroundColor = "blue";
             map["25,25"] = 1;
-
+ 
             document.getElementById("25,25").style.backgroundColor = "blue";
             map["26,26"] = 1;
-
+ 
             document.getElementById("26,26").style.backgroundColor = "blue";
             map["26,27"] = 1;
-
+ 
             document.getElementById("26,27").style.backgroundColor = "blue";
             map["27,26"] = 1;
  
             document.getElementById("27,26").style.backgroundColor = "blue";
             map["27,27"] = 1;
-
+ 
             document.getElementById("27,27").style.backgroundColor = "blue";
-
-            //parts of pattern are off
+ 
+            //parts of other pattern should be turned off 
             map["26,25"] = 0;
             document.getElementById("26,25").style.backgroundColor = "white";
+            map["25,25"] = 0;
+ 
+            document.getElementById("25,25").style.backgroundColor = "white";
+            map["26,26"] = 0;
+ 
+            document.getElementById("26,26").style.backgroundColor = "white";
         }
+ 
+        //play button will keep adding generations up to 23 everytime it is clicked
+        countN();
         
 }
+
+//rules: adding generations
+function countN(){
+// 1. Any live cell with fewer than two live neighbors dies, which is caused by under population.
+// 2. Any live cell with more than three live neighbors dies, as if by overcrowding.
+// 3. Any live cell with two or three live neighbors’ lives on to the next generation.
+// 4. Any dead cell with exactly three live neighbors becomes a live cell.
+// 5. If a dead cell has exactly three live neighbors, it comes to 
+// 6. If a live cell has less than two live neighbors, it dies
+// 7. If a live cell has more than three live neighbors, it dies
+// 8. If a live cell has two or three live neighbors, it continues living. life - Therefore by repeating the 
+// cycle over and over, these simple rules create interesting, often unpredictable patterns.
+
+
+var keys = Object.keys(map);
+var count = 0; 
+console.log(map);
+for(var i = 0; i<50; i++){
+     for(var j = 0; j<50; j++){
+            
+
+             if(map[i+","+j] === 1){
+                if(map[i-1+","+j]=== 1){
+                    count++;
+                    console.log([i-1+","+j]);
+                }
+                if(map[i-1+","+(j+1)]===1){
+                    count++;
+                    console.log([i-1+","+(j+1)]);
+                }
+                
+                if(map[i+","+(j+1)]===1){
+                    count++;
+                    console.log([i+","+(j+1)]);
+                }
+                if(map[i+1+","+(j+1)]===1){
+                    count++;
+                    console.log([i+1+","+(j+1)]);
+                }
+                if(map[i+1+","+j]===1){
+                    count++;
+                    console.log([i+1+","+j]);
+                }
+                if(map[i+1+","+(j-1)]===1){
+                    count++;
+                    console.log([i+1+","+(j-1)]);
+                }
+                if(map[i+","+(j-1)]===1){
+                    count++;
+                    console.log([i+","+(j-1)]);
+                }
+                if(map[i-1+","+(j-1)]===1){
+                    count++;
+                    console.log([i-1+","+(j-1)]);
+                }
+                
+                console.log(count);
+                count = 0;
+                //resets count for next iteration
+                //add rules here 
+             }
+
+     }
+}
+
+// console.log(map);
+// console.log(Object.getOwnPropertyNames(map).length);
+}
+
 
 function stop(){
     var arr = Object.keys(map);
     for(var i = 0; i< arr.length;i++){
         document.getElementById(arr[i]).style.backgroundColor = "white";
-
+ 
     }
 }
-
-//reset button 
+ 
+//reset button to get back to original pattern
 function reset(){
     if(block.checked){
         document.getElementById("24,24").setAttribute('class', 'pattern');
         document.getElementById("24,25").setAttribute('class', 'pattern');
         document.getElementById("25,24").setAttribute('class', 'pattern');
         document.getElementById("25,25").setAttribute('class', 'pattern');
-
+ 
         //off
         document.getElementById("26,26").setAttribute('class', 'none');
         document.getElementById("26,27").setAttribute('class', 'none');
         document.getElementById("27,26").setAttribute('class', 'none');
         document.getElementById("27,27").setAttribute('class', 'none');
-
+ 
         document.getElementById("26,25").setAttribute('class', 'none');
     }
     if(blinker.checked){
         document.getElementById("24,25").setAttribute('class', 'pattern');
         document.getElementById("25,25").setAttribute('class', 'pattern');
         document.getElementById("26,25").setAttribute('class', 'pattern');
-
+ 
         //off
-
+ 
         document.getElementById("24,24").setAttribute('class', 'none');
         document.getElementById("25,24").setAttribute('class', 'none');
-
+ 
         document.getElementById("26,26").setAttribute('class', 'none');
         document.getElementById("26,27").setAttribute('class', 'none');
         document.getElementById("27,26").setAttribute('class', 'none');
@@ -167,14 +248,15 @@ function reset(){
         document.getElementById("24,25").setAttribute('class', 'pattern');
         document.getElementById("25,24").setAttribute('class', 'pattern');
         document.getElementById("25,25").setAttribute('class', 'pattern');
-        document.getElementById("26,26").setAttribute('class', 'pattern');
         document.getElementById("26,27").setAttribute('class', 'pattern');
         document.getElementById("27,26").setAttribute('class', 'pattern');
         document.getElementById("27,27").setAttribute('class', 'pattern');
-
+ 
         //off
-
+ 
         document.getElementById("26,25").setAttribute('class', 'none');
+        document.getElementById("26,26").setAttribute('class', 'none');
+        document.getElementById("25,25").setAttribute('class', 'none');
     }
     for(var i = 0; i<50; i++){
         for(var j = 0; j<50; j++){
@@ -187,18 +269,11 @@ function reset(){
                 document.getElementById(i+","+j).style.backgroundColor = "white";
                 map[i+","+j] = 0;
             }     
-            //console.log(map[i+","+j]);
-        
         }
     }
-
+ 
     
-
-}
-
-//game rules
-function rules(){
-
+ 
 }
 
 genGrid();
